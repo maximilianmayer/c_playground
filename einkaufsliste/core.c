edb0a8269b85aeed27441e0ffd4d6e929c573ceb
Maximilian Mayer <mayer.maximilian@googlemail.com>
2017-01-23 23:05:45 +0100

    fixed menu while loop and implemented writing records to file

--- einkaufsliste/core.c
+++ einkaufsliste/core.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 //#include <time.h>
+//#include <sqlite3.h>
 
 /*
  * Ziel: Eingekaufte Produkte erfassen und in einem Datastore strukturiert hinterlegen.
+ * Datastore: 1. file -> done, 2. sqlite ->tbf, 3. whatever -> not sure if
  *
+ * Step1: Läden anlegen, speichern -> done
+ * Step2: Läden aus Datei(en) auslesen -> WIP
+ * Step3: Rechnungen anlegen & speichern -> tbd
  */
 
 int menu();
@@ -13,6 +18,7 @@ void input_shop();
 void new_shop(char name[], char street[], int *zipcode, char city[]);
 void new_bill();
 void new_product();
+//void shop_write(struct shop *pt);
 
 // 
 struct shop {
@@ -48,42 +54,45 @@ struct product {
 void main() {
  
   int selection;
-  selection=menu();
-  while (selection > '0') {
+  do {
+    selection=menu(); 
+    // debug output
+    //printf("DEBUG: Wert von selection: %d\n",selection);
     switch (selection) {
-      case '0': 
-        printf("goodbye!\n");
-        exit;
-      case '1': 
+      case 0: 
+        printf("Goodbye!\n");
+        break;
+      case 1:
+        //printf("DEBUG: 1 entered %i\n", selection);
         input_shop();
         break;
-      case '2': 
+      case 2:
+        printf("funktion noch nicht implementiert! \n", selection);
         break;
       default: 
         printf("Ungültige Eingabe\n");
-        exit;
+        break;
     }
-    selection=menu();
-    printf("%d\n",selection);
+    /* some debug output -- remove later
+    printf("DEBUG: int-Wert von selection in loop: %d\n",selection);
+    printf("DEBUG: end of loop reached!\n");
+    */
   }
-    printf("%c\n",menu());
+  while (selection > 0);
 }
 
 int menu() {
-  char delim[8]="=======\n";
+  char delim[8]="======\n";
   int select;
   printf("Menu\n%s",delim);  
-  printf("Bitte auswählen:\n");
+  printf("Bitte auswaehlen:\n");
   printf("1: neuen Shop anlegen\n");
   printf("2: neuen Beleg eingeben\n");
   printf("3: neues Produkt anlegen\n");
   printf("0: Beenden\n");
   printf("%sEingabe: ",delim);
-  //scanf("%i",&select);
-  //getchar for 1 digit values!
-  select=getchar();
-  // debug output
-  // printf("%i\n",select);
+  scanf("%i",&select);
+  getchar();
   if (select == 10) select = 0;
   return select;
 }
@@ -131,20 +140,20 @@ void new_shop(char name[], char street[], int *zipcode, char city[]) {
       fprintf(stderr, "Kein Speicherplatz vorhanden fuer neuen shop\n");
       return;
     }
+    memcpy(&shop_pt, &shop_0, sizeof(shop_0));
     // fill struct with function parameters
-    shop_0->id=0;
-    strcpy(shop_0->name,name);
-    strcpy(shop_0->adr_street,street);
-    strcpy(shop_0->adr_city,city);
-    shop_0->adr_zip=*zipcode;
+    shop_pt->id=0;
+    strcpy(shop_pt->name,name);
+    strcpy(shop_pt->adr_street,street);
+    strcpy(shop_pt->adr_city,city);
+    shop_pt->adr_zip=*zipcode;
     // reset pointer to next value as we're on the last
-    shop_0->next=NULL;
-    printf("ID: %i\n",shop_0->id);
-    printf("Name: %s\n",shop_0->name);
+    shop_pt->next=NULL;
   // otherwise hop thorugh chain until shop_pt->next is NULL 
   // */
   }else {
-    shop_pt = shop_0;
+    memcpy(&shop_pt, &shop_0, sizeof(shop_0));
+    //shop_pt = shop_0;
     int i=0;
     while (shop_pt->next != NULL) {
       shop_pt=shop_pt->next;
@@ -164,10 +173,30 @@ void new_shop(char name[], char street[], int *zipcode, char city[]) {
     shop_pt->adr_zip=*zipcode;
     // reset pointer to next value as we're on the last
     shop_pt->next=NULL;
-    printf("ID: %i\n",shop_pt->id);
-    printf("Name: %s\n",shop_pt->name);
+    //shop_write(shop_pt);
+
   }
+  // write record to file
+  char filename[10]="shops.txt";
+  //printf("filename: %s", filename);
+  
+  FILE *file;
+  if ( (file = fopen(filename,"a")) == NULL ) {
+    printf("error: cannot open file: %s\n",filename);
+  }else {
+    printf("Schreibe Datensatz in datei: %s\n",filename);
+    fprintf(file,"ID:%i, Name:%s, Address:%s, zipcode:%i, City:%s\n", shop_pt->id, shop_pt->name, shop_pt->adr_street, shop_pt->adr_zip, shop_pt->adr_city);
+    fclose(file);
+  }
+  
 }
+/*
+void shop_write(struct shop *pt) {
+    printf("ID: %i\n",pt->id);
+    printf("Name: %s\n",pt->name);
+
+}
+*/
 
 void new_bill() {
 }
